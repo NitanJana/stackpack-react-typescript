6c6cefea8f9962ee8d8e9a133baa3477f6a23dda
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-05-11 11:25:48 +0000

    feat: add react-resizable-panels

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
-function App() {
-  return <div>Hello Worlds</div>;
-}
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 
-export default App;
+export default function App() {
+  return (
+    <div className="h-dvh p-2">
+      <PanelGroup direction="horizontal">
+        <Panel>
+          <PanelGroup direction="vertical">
+            <Panel>
+              <div className="h-full border bg-red-100">Code Editor</div>
+            </Panel>
+            <PanelResizeHandle className="h-2 bg-blue-300" />
+            <Panel>
+              <div className="h-full border bg-red-100">Terminal</div>
+            </Panel>
+          </PanelGroup>
+        </Panel>
+        <PanelResizeHandle className="w-2 bg-blue-300" />
+        <Panel>
+          <div className="h-full border bg-red-100">Preview</div>
+        </Panel>
+      </PanelGroup>
+    </div>
+  );
+}
